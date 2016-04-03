71bff730a87e94071572cd6da74ff60eee0cff82
mathiasDJ <music.mathias@gmail.com>
2016-04-03 23:34:41 +0200

    Tooltip on ender seeds

--- src/main/java/io/github/mathiasdj/endercrop/item/ItemEnderSeeds.java
+++ src/main/java/io/github/mathiasdj/endercrop/item/ItemEnderSeeds.java
@@ -1,6 +1,10 @@
 package io.github.mathiasdj.endercrop.item;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 import io.github.mathiasdj.endercrop.init.ModBlocks;
 import io.github.mathiasdj.endercrop.reference.Names;
@@ -10,5 +14,13 @@ public class ItemEnderSeeds extends ItemSeeds {
     public ItemEnderSeeds() {
         super(ModBlocks.blockCropEnder, null);
         this.setUnlocalizedName(Names.Items.SEEDS);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, playerIn, tooltip, advanced);
+        if (stack.getItem() instanceof ItemSeeds) {
+            tooltip.add("Plant on farmland or tilled end stone");
+        }
     }
 }
